791aac5484d08ccf6e23f8313ec7e975bd8fa136
ayushb-developer <ayushb.developer@gmail.com>
2026-03-10 02:39:19 +0530

    feat: Implement Camera following player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(
+            target.position.x,
+            target.position.y + 2,
+            -10
+        );
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+}
