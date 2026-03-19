ea5991fa41a6702a64773ff61e45f680dd7bc285
ayushb-developer <ayushb.developer@gmail.com>
2026-03-19 07:00:06 +0530

    feat : Connect Host and client using Unity Relay

--- Assets/Scripts/Networking/NetworkLauncher.cs
+++ Assets/Scripts/Networking/NetworkLauncher.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Net;
 using System.Net.Sockets;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay.Models;
+using Unity.Services.Relay;
 
 public class NetworkLauncher : MonoBehaviour
 {
@@ -13,23 +17,63 @@ public class NetworkLauncher : MonoBehaviour
     [SerializeField] Button joinButton;
     [SerializeField] TMP_InputField ipAddressField;
     [SerializeField] TMP_Text hostIPText;
+    // void Awake()
+    // {
+    //     hostButton.onClick.AddListener(Host);
+    //     joinButton.onClick.AddListener(Client);
+    // }
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
 
-    void Awake()
-    {
-        hostButton.onClick.AddListener(Host);
-        joinButton.onClick.AddListener(Client);
+        hostButton.onClick.AddListener(CreateRelay);
+        joinButton.onClick.AddListener(() => JoinRelay(ipAddressField.text));
+        // NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
     }
-    void Start()
+
+    async void JoinRelay(string joinCode)
     {
-        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        Debug.Log($"Joining with code: {joinCode}");
+        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        var relayServerData = AllocationUtils.ToRelayServerData(joinAllocation, "dtls");
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+        NetworkManager.Singleton.StartClient();
     }
-    void OnDestroy()
+
+    async void CreateRelay()
     {
-        if (NetworkManager.Singleton != null)
-        {
-            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
-        }
+        Allocation allocation = await Unity.Services.Relay.RelayService.Instance.CreateAllocationAsync(4);
+        string joinCode = await Unity.Services.Relay.RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        Debug.Log($"Join code: {joinCode}");
+        hostIPText.gameObject.SetActive(true);
+        hostIPText.text = "Host : " + joinCode;
+
+        var relayServerData = AllocationUtils.ToRelayServerData(allocation, "dtls");
+        var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartHost();
     }
+    
+    // async void OnApplicationQuit()
+    // {
+    //     if (NetworkManager.Singleton != null)
+    //     {
+    //         if (NetworkManager.Singleton.IsHost)
+    //             NetworkManager.Singleton.StopHost();
+    //         else if (NetworkManager.Singleton.IsClient)
+    //             NetworkManager.Singleton.StopClient();
+    //     }
+    // }
+    // void OnDestroy()
+    // {
+    //     if (NetworkManager.Singleton != null)
+    //     {
+    //         // NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+    //     }
+    // }
 
     private void Host()
     {
@@ -58,13 +102,12 @@ public class NetworkLauncher : MonoBehaviour
     private void Client()
     {   
         var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
-        string ip = "127.0.0.1";
+        string ip = ipAddressField.text;
 #if UNITY_EDITOR
         transport.ConnectionData.Address = "127.0.0.1";
 #else
-
         if (!string.IsNullOrEmpty(ipAddressField.text))
-            transport.ConnectionData.Address = ipAddressField.text;
+            transport.ConnectionData.Address = ip;
 #endif
         Debug.Log($"[JOIN] Trying IP: {ip}:7777");
         NetworkManager.Singleton.StartClient();
